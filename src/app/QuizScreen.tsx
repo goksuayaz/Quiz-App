84db01cad1d0d56d68cf1b8db51b6b228c7c323a
Göksu Ayaz <goksuayaz79@gmail.com>
2024-12-28 20:51:04 +0300

    background changed

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, ImageBackground } from 'react-native'
 import QuestionCard from '../components/QuestionCard'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import Card from '../components/Card';
@@ -8,8 +8,6 @@ import { useEffect } from 'react';
 import { useTimer } from '../hooks/useTimer';
 import LottieView from 'lottie-react-native';
 import party from '../../assets/party.json';
-
-
 
 export default function QuizScreen() {
     const { question, questionIndex, onNext, score, totalQuestions, bestScore } = useQuizContext();
@@ -23,7 +21,6 @@ export default function QuizScreen() {
         }
     }, [question]);
 
-
     useEffect(() => {
         if (time <= 0) {
             onNext();
@@ -32,30 +29,36 @@ export default function QuizScreen() {
 
     console.log("Quiz screen rerender");
 
-
     return (
-
         <SafeAreaView style={styles.page}>
-            <View style={styles.container}>
+            {/* Background */}
+            <View style={styles.background}>
+                <ImageBackground
+                    source={require('../../assets/question.png')}
+                    style={styles.backgroundImage}
+                    resizeMode="cover"
+                />
+            </View>
 
+            <View style={styles.container}>
                 {/* Header */}
                 <View>
                     <Text style={styles.title}> Question {questionIndex + 1}/{totalQuestions} </Text>
                 </View>
 
-
                 {/* Body */}
-
                 {question ? (
                     <View>
                         <QuestionCard question={question} />
                         <Text onPress={clearTimer} style={styles.time} > {time} sec  </Text>
                     </View>
                 ) : (
-
                     <Card title="Well done">
-                        <LottieView style={{ width: 100, height: 100 }} autoPlay
-                            source={require('../../assets/party.json')} />
+                        <LottieView
+                            style={{ width: 100, height: 100 }}
+                            autoPlay
+                            source={require('../../assets/party.json')}
+                        />
                         <Text> Correct answers: {score}/{totalQuestions} </Text>
                         <Text> Best score: {bestScore} </Text>
                     </Card>
@@ -71,7 +74,8 @@ export default function QuizScreen() {
                             size={16}
                             color="white"
                         />
-                    } />
+                    }
+                />
             </View>
         </SafeAreaView>
     )
@@ -79,9 +83,21 @@ export default function QuizScreen() {
 
 const styles = StyleSheet.create({
     page: {
-        backgroundColor: '#FDFEF4',
         flex: 1,
     },
+
+    background: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        opacity: 0.5,
+    },
+    backgroundImage: {
+        flex: 1,
+    },
+
     container: {
         flex: 1,
         justifyContent: "space-between",
@@ -90,14 +106,17 @@ const styles = StyleSheet.create({
 
     title: {
         textAlign: "center",
-        color: "#005055"
+        color: "#005055",
+        fontWeight: "bold",
+        marginTop: 20,
+        fontSize: 20,
     },
+
     time: {
         textAlign: "center",
         marginTop: 15,
         color: "#005055",
         fontWeight: "bold",
-
+        fontSize: 15,
     },
-
-}) 
+});
